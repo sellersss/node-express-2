b49bb85c65cf0838bae3c2ba599e547a0399f2ab
Sellers Crisp <sellershcrisp@gmail.com>
2021-04-05 15:56:20 -0400

    Fix for bug #10

--- bankly/helpers/createToken.js
+++ bankly/helpers/createToken.js
@@ -1,13 +1,19 @@
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 
-
 /** return signed JWT for payload {username, admin}. */
 
-function createToken(username, admin=false) {
-  let payload = {username, admin};
-  return jwt.sign(payload, SECRET_KEY);
-}
+function createToken(username, admin = false) {
+  // FIXES BUG #10
+  // let payload = { username, admin };
+  // return jwt.sign(payload, SECRET_KEY);
 
+  if (username) {
+    let payload = { username, admin };
+    return jwt.sign(payload, SECRET_KEY);
+  } else {
+    return null;
+  }
+}
 
 module.exports = createToken;
